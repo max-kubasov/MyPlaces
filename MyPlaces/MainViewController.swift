a1df027bbe40daa1bbdb71cf07817acad8656bbe
max-kubasov <kubasov87@gmail.com>
2022-10-23 19:21:15 +0300

    Add MainViewController class

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by Max on 23.10.2022.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let restaurantNames = [
+        "Macdolds", "BurgerKing", "Saurma",
+        "Gaga", "LKafa", "OnePrice",
+        "Velmart", "Silpo", "Aushan",
+        "Vkusno i sitno", "Borch", "PubBeer",
+        "VeryWell", "Mafia", "Bistro",
+        "Fransya", "Lviv Kruasan", "Chudo Pekarnya"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
