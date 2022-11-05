282450911f4710e3ef986c0b4c660a344bc0c109
max-kubasov <kubasov87@gmail.com>
2022-11-05 17:59:43 +0200

    Add Map View

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Max on 05.11.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+
+}
